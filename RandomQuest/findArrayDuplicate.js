7c5bc2f6c14ee379324efac05629b8910f1a8222
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-14 19:29:20 +0200

    refactor

--- RandomQuest/findArrayDuplicate.js
+++ RandomQuest/findArrayDuplicate.js
@@ -1,9 +1,3 @@
-//Having an array of Integers 
-//Integers in range 1..n
-//Length: n+1
-//Find function which finds an integer appearing more than once
-//in our array
-
 let testArray = [1,2,3,4,2,4,59,3,32,9];
 
 function isNumberAlreadyInArray(testArray, number){
@@ -20,27 +14,26 @@ function isNumberAlreadyInArray(testArray, number){
 function findRepeatingInteger(testArray){
     // declare array to find repeating integers
     var repeatingIntegers = [];
+    // declare count to recognize if integer repeats
     var count = 0;
     // loop through the array [2 nested loops]
     // check if current item equals second item store it in the array|
     testArray.forEach(element => {
+
         testArray.forEach(secondElement => {
 
             if(element === secondElement && count > 0 && !isNumberAlreadyInArray(repeatingIntegers, element)){
-                console.log(`I reached here as ${element} to be equal to ${secondElement}`)
                 repeatingIntegers.push(element);
                 count = 0;
-                console.log(`2nd: Current count value is ${count}`)
             }
 
             if(element === secondElement){
                 count++;
-                console.log(`1st: Current count value is ${count}`)
             }
 
         })
+
         count = 0;
-        console.log(`3rd: Current count value is ${count}`)
     });
 
     console.log(repeatingIntegers);
