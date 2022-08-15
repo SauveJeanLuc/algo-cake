773d426d0b049d94f60694e646ab057fff8f364e
SauveJeanLuc <sauvejeanluc3@gmail.com>
2022-08-15 17:02:22 +0200

    inplace algo

---
+++ TheoryBag/inplaceAlgo.js
@@ -0,0 +1,8 @@
+/*
+In-Place Algorithms are those that does the operations
+without creating a new copy of the input.
+The changes are visible to other functions.
+Thus being called destructive since it modifies the input during 
+the function call.
+*/
+
